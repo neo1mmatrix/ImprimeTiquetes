0450c88d5382e7d37c5f02a9d6f053a2ffc2f8c4
neo1mmatrix <esteban26mora01@gmail.com>
2020-02-21 09:23:38 -0600

    Agregado Modulo Imprimir

--- ImprimirTiquetes/Form1.Designer.vb
+++ ImprimirTiquetes/Form1.Designer.vb
@@ -22,17 +22,43 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.txtTiquete = New System.Windows.Forms.TextBox()
+        Me.btnImprimir = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'txtTiquete
+        '
+        Me.txtTiquete.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTiquete.Location = New System.Drawing.Point(12, 12)
+        Me.txtTiquete.Multiline = True
+        Me.txtTiquete.Name = "txtTiquete"
+        Me.txtTiquete.Size = New System.Drawing.Size(296, 431)
+        Me.txtTiquete.TabIndex = 0
+        '
+        'btnImprimir
+        '
+        Me.btnImprimir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnImprimir.Location = New System.Drawing.Point(50, 459)
+        Me.btnImprimir.Name = "btnImprimir"
+        Me.btnImprimir.Size = New System.Drawing.Size(217, 32)
+        Me.btnImprimir.TabIndex = 1
+        Me.btnImprimir.Text = "Imprimir"
+        Me.btnImprimir.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(320, 476)
+        Me.ClientSize = New System.Drawing.Size(320, 503)
+        Me.Controls.Add(Me.btnImprimir)
+        Me.Controls.Add(Me.txtTiquete)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Imprime Tiquetes"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents txtTiquete As TextBox
+    Friend WithEvents btnImprimir As Button
 End Class
