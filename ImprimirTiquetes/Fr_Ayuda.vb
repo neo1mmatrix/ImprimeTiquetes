695e1e3f1dd6473bbde0f3f82a544eb772570ffb
Esteban Mora Monge <estemorapz@hotmail.com>
2022-06-23 20:11:39 -0600

    botonCarga los datos de la Impresora

--- ImprimirTiquetes/Fr_Ayuda.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Ayuda
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
-        Me.Close()
-    End Sub
-
-    Private Sub Ayuda_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        Fr_Imprimir.timerCuentaRegresiva.Start()
-    End Sub
-End Class
--- ImprimirTiquetes/My Project/Application.Designer.vb
+++ ImprimirTiquetes/My Project/Application.Designer.vb
@@ -32,7 +32,8 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ImprimirTiquetes.Fr_Imprimir
+
+
         End Sub
     End Class
 End Namespace
